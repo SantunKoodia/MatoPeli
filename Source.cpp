62c022d3747c080e339e9d4ba293f10a351e8034
Santtu <santeriq93@gmail.com>
2023-05-17 22:06:51 +0300

    Add files via upload
    FPS implemented and snake moves

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 
+
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -37,6 +38,120 @@ void Mato::Move() {
 	}
 }
 
+
+//The application time based timer
+class FPSTimer
+{
+	public:
+		//Initializes and functions
+		FPSTimer();
+		void Start();
+		void Stop();
+		void Pause();
+		void Unpause();
+
+		//Gets the timer's time
+		Uint32 getTicks();
+
+		//The timer status
+		bool mPaused;
+		bool mStarted;
+
+	private:
+		//The clock time when the timer started
+		Uint32 mStartTicks;
+
+		//The ticks stored when the timer was paused
+		Uint32 mPausedTicks;
+};
+
+		
+FPSTimer::FPSTimer()
+{
+	//Initialize the variables
+	mStartTicks = 0;
+	mPausedTicks = 0;
+
+	mPaused = false;
+	mStarted = false;
+}
+void FPSTimer::Start()
+{
+	//Start the timer
+	mStarted = true;
+
+	//Unpause the timer
+	mPaused = false;
+
+	//Get the current clock time
+	mStartTicks = SDL_GetTicks();
+	mPausedTicks = 0;
+}
+void FPSTimer::Stop()
+{
+	//Stop the timer
+	mStarted = false;
+
+	//Unpause the timer
+	mPaused = false;
+
+	//Clear tick variables
+	mStartTicks = 0;
+	mPausedTicks = 0;
+}
+void FPSTimer::Pause()
+{
+	//If the timer is running and isn't already paused
+	if (mStarted && !mPaused)
+	{
+		//Pause the timer
+		mPaused = true;
+
+		//Calculate the paused ticks
+		mPausedTicks = SDL_GetTicks() - mStartTicks;
+		mStartTicks = 0;
+	}
+}
+void FPSTimer::Unpause()
+{
+	//If the timer is running and paused
+	if (mStarted && mPaused)
+	{
+		//Unpause the timer
+		mPaused = false;
+
+		//Reset the starting ticks
+		mStartTicks = SDL_GetTicks() - mPausedTicks;
+
+		//Reset the paused ticks
+		mPausedTicks = 0;
+	}
+}
+Uint32 FPSTimer::getTicks()
+{
+	//The actual timer time
+	Uint32 time = 0;
+
+	//If the timer is running
+	if (mStarted)
+	{
+		//If the timer is paused
+		if (mPaused)
+		{
+			//Return the number of ticks when the timer was paused
+			time = mPausedTicks;
+		}
+		else
+		{
+			//Return the current time minus the start time
+			time = SDL_GetTicks() - mStartTicks;
+		}
+	}
+
+	return time;
+}
+
+
 SDL_Window* InitWindow()
 {
 	// initialize SDL window
@@ -71,7 +186,7 @@ SDL_Renderer* InitRenderer(SDL_Window* window)
 	// initialize variable
 	SDL_Renderer* renderer = NULL;
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); // disables VSYNC
 
 	if (renderer == NULL)
 	{
@@ -123,6 +238,21 @@ int main(int argc, char* args[])
 	SDL_Renderer* renderer = NULL;
 	renderer = InitRenderer(window);
 
+	// Frame rate variables
+	int fps = 90;
+	int ScreenTicsPerFrame = 2750 / fps; 
+
+	// Timers to calculate and cap FPS
+	FPSTimer fpsTimer;
+	FPSTimer capTimer;
+
+	// start counting fps
+	int frameCount = 0;
+	fpsTimer.Start();
+
+	// Init Mato class 
+	Mato mato;
+
 	// Check that it was succsefull
 	if (renderer != NULL)
 	{
@@ -134,6 +264,9 @@ int main(int argc, char* args[])
 		// while application is running
 		while (run)
 		{
+			// start/reset cap timer
+			capTimer.Start();
+
 			// Handle events on queue
 			while (SDL_PollEvent(&e) != 0)
 			{
@@ -143,23 +276,62 @@ int main(int argc, char* args[])
 					std::cout << "Quit event called!" << std::endl;
 					run = false;
 				}
+
+				// user presses key
+				else if (e.type == SDL_KEYDOWN)
+				{
+					switch (e.key.keysym.sym) 
+					{
+						case SDLK_UP:
+							mato.direction = UP;
+							break;
+
+						case SDLK_DOWN:
+							mato.direction = DOWN;
+							break;
+
+						case SDLK_LEFT:
+							mato.direction = LEFT;
+							break;
+
+						case SDLK_RIGHT:
+							mato.direction = RIGHT;
+							break;
+
+						//default:
+						// code
+
+					}
+				}
 			}
 
-			// code here
-
 			// render background 
-			SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+			SDL_SetRenderDrawColor(renderer, 45, 180, 0, 255);
 			// clear window
 			SDL_RenderClear(renderer);
 
-			// Mato class here
-			Mato mato;
+			// Snake and Food stuff here
+
+			mato.Move();
 
 			//DrawRectangle(renderer, rect);
 			DrawSnake(renderer, mato);
 
+			// calculate and correct fps
+			float avgFps = frameCount / (fpsTimer.getTicks() / 1000.f);
+			if (avgFps > 2000000) {
+				avgFps = 0;
+			}
+
 			// Render to screen
 			SDL_RenderPresent(renderer);
+
+			frameCount++;
+			int frameTicks = capTimer.getTicks();
+			if (frameTicks < ScreenTicsPerFrame) {
+				// wait remaining time
+				SDL_Delay(ScreenTicsPerFrame - frameTicks);
+			}
 		}
 
 		//SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
@@ -168,7 +340,7 @@ int main(int argc, char* args[])
 	// free memory
 	Close(window, renderer);
 
-	std::cout << NONE << LEFT << RIGHT << UP << DOWN << "done" << std::endl;
+	//std::cout << NONE << LEFT << RIGHT << UP << DOWN << "done" << std::endl;
 
 	std::cout << "End program" << std::endl;
 	return 0;
